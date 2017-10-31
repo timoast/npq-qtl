43055664e9175e8b45be76082d991308a1e5f723
timoast <timstuart90@gmail.com>
2017-10-31 10:23:24 +0800

    keep sra code

--- code/rename_files.py
+++ code/rename_files.py
@@ -4,18 +4,12 @@ import os
 from glob import glob
 
 
-data = {}
-with open("../RawData/accession_sra.tsv", "r") as infile:
-    for line in infile:
-        line = line.rsplit()
-        data[line[0]] = [line[1], line[2]]
-
 for key, value in data.items():
     os.makedirs("../ProcessedData/" + key)
     fastqs = glob("./" + data[key][1] + "*/*.fastq.gz")
     for i in fastqs:
         trailing = i.split("/")[-1]
-        os.rename(i, str("../ProcessedData/" + data[key][1] + "/" + trailing))
+        os.rename(i, str("../ProcessedData/" + key + "/" + trailing))
 
 for i in glob("./SRR*"):
     if not os.listdir(i):
