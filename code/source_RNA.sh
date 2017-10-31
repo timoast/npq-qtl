ec875336c67fc6d04ca5cc3d5df48554ef695e98
timoast <timstuart90@gmail.com>
2017-10-31 14:40:04 +0800

    Put output in rawdata

--- code/source_RNA.sh
+++ code/source_RNA.sh
@@ -1,1 +1,1 @@
-wget ftp://ftp.ncbi.nlm.nih.gov//geo/series/GSE80nnn/GSE80744/suppl/GSE80744_ath1001_tx_norm_2016-04-21-UQ_gNorm_normCounts_k4.tsv.gz
+wget -P ../RawData/ ftp://ftp.ncbi.nlm.nih.gov//geo/series/GSE80nnn/GSE80744/suppl/GSE80744_ath1001_tx_norm_2016-04-21-UQ_gNorm_normCounts_k4.tsv.gz
