edee2d8eec4f6bbaff22db90951e4480afcfb1ac
timoast <timstuart90@gmail.com>
2017-11-01 14:04:07 +0800

    Add script to rename sra files

---
+++ code/rename_sra.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import os
+import sys
+from glob import glob
+
+
+names = {}
+with open(sys.argv[1], "r") as infile:
+    for line in infile:
+        line = line.rsplit()
+        names[line[0]] = line[2]
+
+files = glob("*_npq.bam*")
+for i in files:
+    sra = i.split("_")[0]
+    os.rename(i, names[sra] + i.strip(sra)[1])
